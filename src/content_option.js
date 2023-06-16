fabea4622ec9dfd887418e23830ebd50b2790d94
suvo-khan <mdsuvoshek1@gmail.com>
2023-06-16 16:16:20 +0600

    added

--- src/content_option.js
+++ src/content_option.js
@@ -1,75 +1,103 @@
-const logotext = "JOHN";
+const logotext = "SUVO";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "MD Rabby Shek Suvo",
+    description: "I’m Suvo data scientist _ Full stack devloper,currently working in Dhaka",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m MD Rabby Shek Suvo",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop web applications",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Inspiring Solutions, Empowering Experiences.",
+    your_img_url: "https://wallpaperaccess.com/full/35370.jpg",
 };
 
 const dataabout = {
     title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    aboutme: "Welcome to my portfolio! I am a passionate and skilled [your profession] with a strong dedication to creativity and innovation. With 3 years of experience, I have honed my expertise in [specific skills or areas of focus]. Through my work, I strive to deliver impactful solutions that not only meet clients' needs but also exceed their expectations. From designing captivating user interfaces to crafting efficient backend systems, I am committed to creating seamless and engaging experiences. Explore my portfolio to witness the fusion of my technical skills, artistic flair, and unwavering commitment to excellence. Let's bring your vision to life!",
 };
 const worktimeline = [{
         jobtitle: "Designer of week",
-        where: "YAdfi",
+        where: "Dhaka",
         date: "2020",
     },
     {
         jobtitle: "Designer of week",
-        where: "Jamalya",
+        where: "Dhaka",
         date: "2019",
     },
     {
         jobtitle: "Designer of week",
-        where: "ALquds",
+        where: "Dhaka",
         date: "2019",
     },
 ];
 
 const skills = [{
-        name: "Python",
-        value: 90,
+        name: "HTML",
+        value: 95,
     },
     {
-        name: "Djano",
+        name: "CSS",
         value: 85,
     },
     {
         name: "Javascript",
-        value: 80,
+        value: 90,
     },
     {
-        name: "React",
-        value: 60,
+        name: "Reactjs",
+        value: 70,
     },
     {
         name: "Jquery",
         value: 85,
     },
+    {
+        name: "Bootstrap",
+        value: 90,
+    },
+    {
+        name: "php",
+        value: 75,
+    },
+    {
+        name: "C",
+        value: 60,
+    },
+    {
+        name: "C++",
+        value: 60,
+    },
+    {
+        name: "python",
+        value: 60,
+    },
+    {
+        name: "Java",
+        value: 60,
+    },
+    {
+        name: "MySql",
+        value: 75,
+    },
 ];
 
 const services = [{
         title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+        description: "Creating seamless and intuitive user interfaces that enhance user experiences and drive engagement.",
     },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+    // {
+    //     title: "web application",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Web design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
 ];
 
 const dataportfolio = [{
@@ -136,20 +164,20 @@ const dataportfolio = [{
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "rabbyshek1998@gmail.com",
+    YOUR_FONE: "01724707680",
+    description: "contact with me.",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+   // YOUR_SERVICE_ID: "service_id",
+   // YOUR_TEMPLATE_ID: "template_id",
+   // YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
+    github: "https://github.com/",
     facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
+    linkedin: "https://www.linkedin.com/in/md-rabby-shek-suvo-1739aa224/",
     twitter: "https://twitter.com",
 };
 export {
